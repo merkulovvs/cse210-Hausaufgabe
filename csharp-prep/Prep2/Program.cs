418ebfb5033022829c0918b0ac2a61caa4e78763
Victor Merkulov <18528771+merkulovvs@users.noreply.github.com>
2024-01-30 21:46:59 +0100

    Completed Prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "EPAM";
+        job1._startYear = 2007;
+        job1._endYear = 2010;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Software Engineer";
+        job2._company = "Luxsoft";
+        job2._startYear = 2010;
+        job2._endYear = 2019;
+
+        Resume myResume = new Resume();
+        myResume._name = "Victor Merkulov";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();          
+    
+
     }
 }
