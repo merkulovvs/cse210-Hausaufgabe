da5249182ae10600518903d5cb10942653abb458
Victor Merkulov <18528771+merkulovvs@users.noreply.github.com>
2024-02-07 22:56:27 +0100

    completed week 5 prep assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square sqr = new Square("black",2);
+        //Console.WriteLine(sqr.GetColor());
+        //Console.WriteLine(sqr.GetArea());
+        shapes.Add(sqr);
+
+        Rectangle rec = new Rectangle("brown", 2, 4);
+        //Console.WriteLine(rec.GetColor());
+        //Console.WriteLine(rec.GetArea());
+        shapes.Add(rec);
+
+        Circle cir = new Circle("yellow", 6);
+        //Console.WriteLine(cir.GetColor());
+        //Console.WriteLine(cir.GetArea());
+        shapes.Add(cir);
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"A {color} shape has area {area}");
+        }
+
     }
 }
